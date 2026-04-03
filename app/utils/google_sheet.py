223b06aff5173782229d894b2450812acf1d89f5
muhammadwithskilled-gif <muhammadskilled@gmail.com>
2026-04-04 03:34:35 +0500

    Use environment variable for Google credentials

--- app/utils/google_sheet.py
+++ app/utils/google_sheet.py
@@ -1,35 +1,43 @@
-# # app/utils/google_sheet.py
+# app/utils/google_sheet.py
 
-# import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
+import os
+import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-# # Google API scope
-# scope = [
-#     "https://spreadsheets.google.com/feeds",
-#     "https://www.googleapis.com/auth/drive"
-# ]
+# Google API scope
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
 
-# # Load credentials
-# creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
-# client = gspread.authorize(creds)
+# ------------------------------
+# Load credentials from Environment Variable
+# ------------------------------
+creds_json = os.environ.get("GOOGLE_CREDENTIALS")  # <- This is the Render env variable
+if not creds_json:
+    raise Exception("GOOGLE_CREDENTIALS environment variable not set!")
 
-# # Open your sheet
-# sheet = client.open("Orders").sheet1
+creds_dict = json.loads(creds_json)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+client = gspread.authorize(creds)
+
+# Open your Google Sheet
+sheet = client.open("Orders").sheet1
 
 
-# def save_order_to_sheet(order):
-#     """
-#     Save order data into Google Sheet
-#     """
-
-#     sheet.append_row([
-#         order.get("product_name"),
-#         order.get("product_detail"),
-#         order.get("product_description"),
-#         order.get("delivery_time"),
-#         order.get("payment_method"),
-#         order.get("customer_name"),
-#         order.get("customer_address"),
-#         order.get("delivery_date"),
-#         order.get("status")
-#     ])
+def save_order_to_sheet(order):
+    """
+    Save order data into Google Sheet
+    """
+    sheet.append_row([
+        order.get("product_name"),
+        order.get("product_detail"),
+        order.get("product_description"),
+        order.get("delivery_time"),
+        order.get("payment_method"),
+        order.get("customer_name"),
+        order.get("customer_address"),
+        order.get("delivery_date"),
+        order.get("status")
+    ])
