22e248f4106dc112c3ee303651bd210c78352ee7
muhammadwithskilled-gif <muhammadskilled@gmail.com>
2026-04-11 02:12:37 +0500

    fix google credentials env issue

--- app/utils/google_sheet.py
+++ app/utils/google_sheet.py
@@ -1,35 +1,23 @@
-# app/utils/google_sheet.py
-
 import os
 import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-# Google API scope
 scope = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive"
 ]
 
-# ------------------------------
-# Load credentials from Environment Variable
-# ------------------------------
-creds_json = os.environ.get("GOOGLE_CREDENTIALS")  # <- This is the Render env variable
-if not creds_json:
-    raise Exception("GOOGLE_CREDENTIALS environment variable not set!")
+# READ FROM ENVIRONMENT VARIABLE
+creds_dict = json.loads(os.environ.get("GOOGLE_CREDENTIALS"))
 
-creds_dict = json.loads(creds_json)
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
 
-# Open your Google Sheet
 sheet = client.open("Orders").sheet1
 
 
 def save_order_to_sheet(order):
-    """
-    Save order data into Google Sheet
-    """
     sheet.append_row([
         order.get("product_name"),
         order.get("product_detail"),
